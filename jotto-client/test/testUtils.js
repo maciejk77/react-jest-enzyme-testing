6b6b83db1d9ae55009d41801ec768cb3e54c3516
Maciej Kurek <maciejk77@gmail.com>
2020-11-23 14:51:34 +0000

    Add thunk middleware to testUtils, make test passing

--- jotto-client/test/testUtils.js
+++ jotto-client/test/testUtils.js
@@ -1,8 +1,9 @@
 import rootReducer from '../src/reducers';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, applyMiddleware(thunk));
 };
 
 export const findByTestAttr = (wrapper, value) =>
