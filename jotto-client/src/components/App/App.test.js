dad4d7459db266147513338779998e1e3f8c9b82
Maciej Kurek <maciejk77@gmail.com>
2020-11-26 09:19:51 +0000

    Basic App, context and hooks path

--- jotto-client/src/components/App/App.test.js
+++ jotto-client/src/components/App/App.test.js
@@ -1,57 +1,70 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../../test/testUtils';
+import App from './';
 
-import { storeFactory } from '../../../test/testUtils';
-import App, { UnconnectedApp } from './';
+const setup = () => shallow(<App />);
 
-const setup = (state = {}) => {
-  const store = storeFactory(state);
-
-  const wrapper = shallow(<App store={store} />)
-    .dive()
-    .dive();
-  return wrapper;
-};
-
-describe('redux properties', () => {
-  it('has access to `success` state', () => {
-    const success = true;
-    const wrapper = setup({ success });
-    const successProp = wrapper.instance().props.success;
-    expect(successProp).toBe(success);
-  });
-
-  it('has access to `secretWord` state', () => {
-    const secretWord = 'party';
-    const wrapper = setup({ secretWord });
-    const secretWordProp = wrapper.instance().props.secretWord;
-    expect(secretWordProp).toBe(secretWord);
-  });
-
-  it('has access to `guessedWords` state', () => {
-    const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
-    const wrapper = setup({ guessedWords });
-    const guessedWordsProp = wrapper.instance().props.guessedWords;
-    expect(guessedWordsProp).toEqual(guessedWords);
-  });
-
-  it('`getSecretWord` action creator is a function on the props', () => {
-    const wrapper = setup();
-    const getSecretWordProp = wrapper.instance().props.getSecretWord;
-    expect(getSecretWordProp).toBeInstanceOf(Function);
-  });
+it('App renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
 });
 
-it('`getSecretWordMock` runs on App mount', () => {
-  const getSecretWordMock = jest.fn();
-  const props = {
-    getSecretWord: getSecretWordMock,
-    success: false,
-    guessedWords: [],
-  };
+// import React from 'react';
+// import { shallow } from 'enzyme';
 
-  const wrapper = shallow(<UnconnectedApp {...props} />);
-  wrapper.instance().componentDidMount();
-  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
-  expect(getSecretWordCallCount).toBe(1);
-});
+// import { storeFactory } from '../../../test/testUtils';
+// import App, { UnconnectedApp } from './';
+
+// const setup = (state = {}) => {
+//   const store = storeFactory(state);
+
+//   const wrapper = shallow(<App store={store} />)
+//     .dive()
+//     .dive();
+//   return wrapper;
+// };
+
+// describe('redux properties', () => {
+//   it('has access to `success` state', () => {
+//     const success = true;
+//     const wrapper = setup({ success });
+//     const successProp = wrapper.instance().props.success;
+//     expect(successProp).toBe(success);
+//   });
+
+//   it('has access to `secretWord` state', () => {
+//     const secretWord = 'party';
+//     const wrapper = setup({ secretWord });
+//     const secretWordProp = wrapper.instance().props.secretWord;
+//     expect(secretWordProp).toBe(secretWord);
+//   });
+
+//   it('has access to `guessedWords` state', () => {
+//     const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+//     const wrapper = setup({ guessedWords });
+//     const guessedWordsProp = wrapper.instance().props.guessedWords;
+//     expect(guessedWordsProp).toEqual(guessedWords);
+//   });
+
+//   it('`getSecretWord` action creator is a function on the props', () => {
+//     const wrapper = setup();
+//     const getSecretWordProp = wrapper.instance().props.getSecretWord;
+//     expect(getSecretWordProp).toBeInstanceOf(Function);
+//   });
+// });
+
+// it('`getSecretWordMock` runs on App mount', () => {
+//   const getSecretWordMock = jest.fn();
+//   const props = {
+//     getSecretWord: getSecretWordMock,
+//     success: false,
+//     guessedWords: [],
+//   };
+
+//   const wrapper = shallow(<UnconnectedApp {...props} />);
+//   wrapper.instance().componentDidMount();
+//   const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+//   expect(getSecretWordCallCount).toBe(1);
+// });
--- jotto-client/src/components/App/index.js
+++ jotto-client/src/components/App/index.js
@@ -1,30 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import GuessedWords from '../GuessedWords';
-import Congrats from '../Congrats';
-import Input from '../Input';
-import { getSecretWord } from '../../actions';
+import React from 'react';
 
-export class UnconnectedApp extends Component {
-  componentDidMount() {
-    this.props.getSecretWord();
-  }
-  render() {
-    return (
-      <div>
-        <h1>Jotto</h1>
-        <div>Secret word is: {this.props.secretWord}</div>
-        <Input />
-        <Congrats success={this.props.success} />
-        <GuessedWords guessedWords={this.props.guessedWords} />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const { success, guessedWords, secretWord } = state;
-  return { success, guessedWords, secretWord };
+const App = () => {
+  return (
+    <div data-test="component-app">
+      <h1>Jotto</h1>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
+export default App;
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import GuessedWords from '../GuessedWords';
+// import Congrats from '../Congrats';
+// import Input from '../Input';
+// import { getSecretWord } from '../../actions';
+
+// export class UnconnectedApp extends Component {
+//   componentDidMount() {
+//     this.props.getSecretWord();
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Jotto</h1>
+//         <div>Secret word is: {this.props.secretWord}</div>
+//         <Input />
+//         <Congrats success={this.props.success} />
+//         <GuessedWords guessedWords={this.props.guessedWords} />
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   const { success, guessedWords, secretWord } = state;
+//   return { success, guessedWords, secretWord };
+// };
+
+// export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
